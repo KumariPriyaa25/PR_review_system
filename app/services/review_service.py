3b7195d843d7b3efc9a75acbe116a9d51ea54a65
kumari priya <priyaasinghh2508@gmail.com>
2026-07-10 23:00:13 +0530

    fix review_service imports

--- app/services/review_service.py
+++ app/services/review_service.py
@@ -1,9 +1,19 @@
+from app.github.diff_parser import DiffParser
 from app.github.pr_fetcher import PRFetcher
 
 
 def process_pull_request(owner , repo , pr_number):
     print(f"Starting review workflow for PR #{pr_number}")
     fetcher = PRFetcher()
-    result = fetcher.fetch(owner, repo, pr_number)
-    print(f"Fetched changed files for PR #{pr_number}")
-    return result
+    # result = fetcher.fetch(owner, repo, pr_number)
+    # print(f"Fetched changed files for PR #{pr_number}")
+    # return result
+    review = fetcher.fetch(owner, repo, pr_number)
+
+    parser = DiffParser()
+
+    parsed = parser.parse(review["files"])
+
+    review["parsed_files"] = parsed
+
+    return review
